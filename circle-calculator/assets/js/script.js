15b1014fab20abfebb0194a0ae476b6179ed44f2
Madrosey13 <143127672+Madrosey13@users.noreply.github.com>
2025-01-30 20:54:59 +0000

    Updated Circle Calculations

--- circle-calculator/assets/js/script.js
+++ circle-calculator/assets/js/script.js
@@ -1,3 +1,32 @@
+"use strict";
 $( "#CircleForm" ).validate({
 
 });
+
+function CircleCalculation() {
+    if ($( "#CircleForm" ).valid()){
+        let radius;
+        let diameter;
+        let circumference;
+        let radiusfp;
+
+        radius = document.getElementById("radius").value;
+        radiusfp = parseFloat(radius);
+
+        diameter = calculateDiameter(radiusfp);
+        document.getElementById("diameter").innerHTML = diameter;
+
+        circumference = calculateCircumference(radiusfp);
+        document.getElementById("circumference").innerHTML = circumference;
+        let x = 3;
+    }
+}
+
+function calculateDiameter(r){
+    return 2 * r;
+}
+
+function calculateCircumference(r){
+
+    return 2 * Math.PI * r;
+}
